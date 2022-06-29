1c8bbc7ead330073cc6853cbdf168661068fd7c4
lily <raichek23@gmail.com>
2022-06-30 00:35:08 +0900

    Add delete proper

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -34,4 +34,9 @@ export class MembersComponent implements OnInit {
     this.memberService.addMember({ name } as Member)
       .subscribe(member => { this.members.push(member)});
   }
+
+  delete(member: Member): void {
+    this.members = this.members.filter(m => m !== member);
+    this.memberService.deleteMember(member).subscribe();
+  }
 }
